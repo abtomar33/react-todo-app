401f7e292a6cef338308e40c2409a42fbcb6dd07
ABHISHEK TOMER <abhishek@TSPI.TECH>
2020-08-12 20:25:42 +0530

    Updates

---
+++ precache-manifest.d1e9b0d81fe4c6cad84413a110feca17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31094c4ba606c443e503e06c2ff4635b",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "5026a970827e536bcdf7",
+    "url": "/react-todo-app/static/css/main.d45e9c4f.chunk.css"
+  },
+  {
+    "revision": "3fa6d5a02ddc0c58f0da",
+    "url": "/react-todo-app/static/js/2.de96f8a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.de96f8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5026a970827e536bcdf7",
+    "url": "/react-todo-app/static/js/main.f843e8b0.chunk.js"
+  },
+  {
+    "revision": "1528fb5b7daed31266f4",
+    "url": "/react-todo-app/static/js/runtime-main.1dd0cb46.js"
+  }
+]);
